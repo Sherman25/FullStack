06d953ab1e08e18d15a7ab200c3942dfc3826687
Sherman25 <n.v.sherman@gmail.com>
2021-04-19 15:58:07 +0300

    init functional components

--- hw04/src/pages/App.js
+++ hw04/src/pages/App.js
@@ -1,6 +1,26 @@
+import React from 'react';
 import '../App.css';
 
-function App() {
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            "user": {},
+            "posts": [],
+            "popular": [],
+            "latest": [],
+        }
+    }
+
+    render(){
+        return (
+          <div>
+              <Header/>
+              <MainSection/>
+              <SideBar/>
+          </div>
+        );
+    }
 }
 
 export default App;
