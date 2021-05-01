f5c6d60806800418562327a9d49285c0e8ee5d18
Sherman25 <n.v.sherman@gmail.com>
2021-05-01 05:12:10 +0300

    login page implementation

--- hw05/src/components/Login.js
+++ hw05/src/components/Login.js
@@ -1,9 +1,48 @@
-function Login(){
-    return(
-        <dev>
-            Login page
-        </dev>
-    );
+import React, {Component} from 'react';
+import CustomInput from './CustomInput';
+import Button from './Button';
+import './Login.css';
+
+class Login extends React.Component {
+    state = {
+        email: "",
+        password: ""
+    };
+
+    handleChange = e => {
+        this.setState({ [e.currentTarget.id]: e.currentTarget.value });
+    };
+    render() {
+        return (
+            <dev>
+                Login page
+                <form className="form">
+                    <CustomInput
+                        labelText="Email"
+                        id="email"
+                        formControlProps={{
+                            fullWidth: true
+                        }}
+                        handleChange={this.handleChange}
+                        type="text"
+                    />
+                    <CustomInput
+                        labelText="Password"
+                        id="password"
+                        formControlProps={{
+                            fullWidth: true
+                        }}
+                        handleChange={this.handleChange}
+                        type="password"
+                    />
+
+                    <Button type="button" color="primary" className="form__custom-button">
+                        Log in
+                    </Button>
+                </form>
+            </dev>
+        );
+    }
 }
 
 export default Login;
